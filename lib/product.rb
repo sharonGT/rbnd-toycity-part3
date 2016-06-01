25a66b3e5e566cd802c2a5192dfb38f20ccfe009
Sharon Gozcu <sharongozcu@gmail.com>
2016-06-02 00:38:52 +0200

    created add_to_products method to raise and error if duplicate products are added

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,28 @@
 class Product
+	attr_reader :title, :price, :stock
 	@@products = []
 
 	def initialize(options={})
-		@@products << self
+		@title = options[:title]
+		@price = options[:price]
+		@stock = options[:stock]
+		add_to_products
 	end
 
 	def self.all
 		@@products
+	end
+
+	
+	def add_to_products 
+		if @@products.map { |product| product.title }.include? @title
+			raise DuplicateProductError, "#{@title} already exists."
+		else
+		@@products << self
+		end
+	end
+
+	def self.find_by_title(nameOfProduct)
+		@@products.find{|eachName| eachName.title == nameOfProduct}
 	end
 end
