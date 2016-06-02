405328307a79938725196b9675cbb7a0bf5d0332
Sharon Gozcu <sharongozcu@gmail.com>
2016-06-02 17:00:53 +0200

    created in_stock? method and verified that nanoblock.title, nanoblock.price, nanoblock.stock works

--- lib/product.rb
+++ lib/product.rb
@@ -13,16 +13,25 @@ class Product
 		@@products
 	end
 
+	def self.find_by_title(title)
+		@@products.each do |product|
+			if product.title == title
+				return product
+			end
+		end
+	end
+
+	def in_stock?
+		@stock > 0
+	end
 	
+	private
+
 	def add_to_products 
 		if @@products.map { |product| product.title }.include? @title
 			raise DuplicateProductError, "#{@title} already exists."
 		else
 		@@products << self
 		end
-	end
-
-	def self.find_by_title(nameOfProduct)
-		@@products.find{|eachName| eachName.title == nameOfProduct}
 	end
 end
