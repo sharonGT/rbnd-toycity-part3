591b4deb2640aaf78e96027014a9dbb827c46ed3
Sharon Gozcu <sharongozcu@gmail.com>
2016-06-02 17:10:45 +0200

    created self.in_stock method to return an array of all products with a stock greater than zero. Verified that products_in_stock works for both nanoblock and firehouse.

--- lib/product.rb
+++ lib/product.rb
@@ -24,7 +24,17 @@ class Product
 	def in_stock?
 		@stock > 0
 	end
-	
+
+	def self.in_stock
+		stock_list = []
+		@@products.each do |product|
+			if product.stock > 0
+				stock_list << product
+			end
+		end
+		return stock_list
+	end
+
 	private
 
 	def add_to_products 
